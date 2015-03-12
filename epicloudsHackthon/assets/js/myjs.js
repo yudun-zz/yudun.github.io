edfc0c5eff91dfba347d3b66b364e74ab357abe7
yudun <wyudun@gmail.com>
2015-03-12 20:40:28 +0800

    完成like和收藏点击的js

---
+++ epicloudsHackthon/assets/js/myjs.js
@@ -0,0 +1,18 @@
+
+$(".zan, .collect").click(function(){
+	var clicked = $(this).attr("clicked")
+	var ori = $(this).html()
+	var num = /(\d+)/.exec(ori)[1]
+
+	if(clicked != "true"){
+		$(this).html(ori.replace(num, num-0+1))
+		$(this).css("color","#EF189B")
+		$(this).attr("clicked", "true")
+	}
+	else{
+		$(this).html(ori.replace(num, num-1))
+		$(this).css("color","#999999")
+		$(this).attr("clicked", "false")
+	}
+})
+
---
+++ epicloudsHackthon/assets/js/template.js
@@ -0,0 +1,59 @@
+
+  $(function() {
+    var $tpl = $('#amz-tpl'),
+      source = $tpl.text(),
+      template = Handlebars.compile(source),
+      data = {
+        header: {
+          "content": {
+            "title": "萌宝圈",
+            "link": "index.html",
+            "right": [
+              {
+                "link": "#right-link",
+                "icon": "camera"
+              }],
+          },
+          "options": {
+            "fixed": true
+          }
+        },
+        navbar: {
+          "options": {
+            "cols": "4",
+            "iconpos": "top"
+          },
+          "content": [
+            {
+              "title": "萌宝圈",
+              "link": "index.html",
+              "icon": "child",
+              "dataApi": ""
+            },
+            {
+              "title": "个人中心",
+              "link": "",
+              "icon": "user",
+              "dataApi": "data-am-navbar-qrcode"
+            },
+            {
+              "title": "分享",
+              "link": "",
+              "icon": "share",
+              "dataApi": "data-am-navbar-share"
+            },
+
+          ]
+        }
+      },
+      html = template(data);
+
+    $tpl.before(html);
+
+    // var reflow = $('body')[0].offsetHeight;
+
+    $.each([], function(i, m) {
+      var module = $.AMUI[m];
+      module && module.init && module.init();
+    })
+  });
